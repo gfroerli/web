79e1318bdb172d3a54da21d38f3765e03c507664
Danilo Bargen <mail@dbrgn.ch>
2017-11-22 21:34:54 +0100

    Add CSS reset (fixes #1)

--- src/Stylesheets.elm
+++ src/Stylesheets.elm
@@ -1,16 +1,25 @@
 port module Stylesheets exposing (..)
 
 import AppCss
+import Css exposing (Stylesheet)
 import Css.File exposing (..)
+import Css.Reset
 
 
 port files : CssFileStructure -> Cmd msg
 
 
+styles : List Stylesheet
+styles =
+    [ Css.Reset.css
+    , AppCss.css
+    ]
+
+
 fileStructure : CssFileStructure
 fileStructure =
     Css.File.toFileStructure
-        [ ( "styles.css", Css.File.compile [ AppCss.css ] ) ]
+        [ ( "styles.css", Css.File.compile styles ) ]
 
 
 main : CssCompilerProgram
