51d7dd7083f4743e4701b3c48fc6b24323fdfc0a
Danilo Bargen <mail@dbrgn.ch>
2017-11-27 00:16:41 +0100

    Add sensor decoder tests

--- tests/ApiTests.elm
+++ tests/ApiTests.elm
@@ -1,5 +1,6 @@
 module ApiTests exposing (..)
 
+import Json.Decode as Decode
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Http
@@ -10,11 +11,86 @@ import Test exposing (..)
 suite : Test
 suite =
     describe "API"
-        [ describe "Helper functions"
+        [ describe "Authentication"
             [ test "getHeaders" <|
                 \() ->
                     Expect.equal
                         (getHeaders "foo")
                         [ Http.header "Authorization" "Bearer foo" ]
+            ]
+        , describe "Decoders"
+            [ describe "sensorDecoder"
+                [ test "Valid" <|
+                    \() ->
+                        let
+                            result =
+                                Decode.decodeString sensorDecoder
+                                    """
+                                    { "id": 1
+                                    , "device_name": "foo"
+                                    , "caption": "A nice sensor."
+                                    }
+                                    """
+                        in
+                            Expect.equal result <|
+                                Ok (Sensor 1 "foo" (Just "A nice sensor."))
+                , test "Null caption" <|
+                    \() ->
+                        let
+                            result =
+                                Decode.decodeString sensorDecoder
+                                    """
+                                    { "id": 1
+                                    , "device_name": "foo"
+                                    , "caption": null
+                                    }
+                                    """
+                        in
+                            Expect.equal result <|
+                                Ok (Sensor 1 "foo" Nothing)
+                , test "Extra fields" <|
+                    \() ->
+                        let
+                            result =
+                                Decode.decodeString sensorDecoder
+                                    """
+                                    { "id": 1
+                                    , "device_name": "foo"
+                                    , "caption": null
+                                    , "blergh": "blubh"
+                                    }
+                                    """
+                        in
+                            Expect.equal result <|
+                                Ok (Sensor 1 "foo" Nothing)
+                , test "No caption" <|
+                    \() ->
+                        let
+                            result =
+                                Decode.decodeString sensorDecoder
+                                    """
+                                    { "id": 1
+                                    , "device_name": "foo"
+                                    }
+                                    """
+                        in
+                            Expect.equal
+                                result
+                                (Err """Expecting an object with a field named `caption` but instead got: {"id":1,"device_name":"foo"}""")
+                , test "No id" <|
+                    \() ->
+                        let
+                            result =
+                                Decode.decodeString sensorDecoder
+                                    """
+                                    { "device_name": "foo"
+                                    , "caption": null
+                                    }
+                                    """
+                        in
+                            Expect.equal
+                                result
+                                (Err """Expecting an object with a field named `id` but instead got: {"device_name":"foo","caption":null}""")
+                ]
             ]
         ]
