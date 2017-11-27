5cdff6d820f0409f3b3fcd2744552e3de02273b6
Danilo Bargen <mail@dbrgn.ch>
2017-11-27 23:37:26 +0100

    Expand sensor decoder

--- src/Api.elm
+++ src/Api.elm
@@ -2,7 +2,9 @@ module Api exposing (getHeaders, Sensor, sensorDecoder)
 
 import Http
 import Json.Decode as Decode
+import Json.Decode.Extra as DecodeExtra
 import Json.Decode.Pipeline as Pipeline
+import Date exposing (Date)
 
 
 -- AUTHENTICATION
@@ -21,6 +23,11 @@ type alias Sensor =
     { id : Int
     , deviceName : String
     , caption : Maybe String
+    , latitude : Float
+    , longitude : Float
+    , sponsorId : Maybe Int
+    , createdAt : Date
+    , updatedAt : Date
     }
 
 
@@ -30,3 +37,8 @@ sensorDecoder =
         |> Pipeline.required "id" Decode.int
         |> Pipeline.required "device_name" Decode.string
         |> Pipeline.required "caption" (Decode.nullable Decode.string)
+        |> Pipeline.required "latitude" Decode.float
+        |> Pipeline.required "longitude" Decode.float
+        |> Pipeline.required "sponsor_id" (Decode.nullable Decode.int)
+        |> Pipeline.required "created_at" DecodeExtra.date
+        |> Pipeline.required "updated_at" DecodeExtra.date
