490e2f09e81241e0502d08cf108a44bcd80eff6c
Danilo Bargen <mail@dbrgn.ch>
2018-06-11 23:26:06 +0200

    Update copyright year (#26)
    Fixes #22.

--- src/Views.elm
+++ src/Views.elm
@@ -178,7 +178,7 @@ mapView model =
                 , padding2 (px 8) zero
                 ]
             ]
-            [ text "© 2017 Coredump Rapperswil-Jona"
+            [ text "© 2017–2018 Coredump Rapperswil-Jona"
             , text " | "
             , a [ href Routing.aboutPath ] [ text "About" ]
             , text " | "
