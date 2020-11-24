5fa60cfdc47b5b5a769e13b5b25b9268453b3f0a
Danilo Bargen <mail@dbrgn.ch>
2020-11-25 00:31:12 +0100

    Update copyright year in footer

--- src/elm/Views.elm
+++ src/elm/Views.elm
@@ -243,7 +243,7 @@ mapView model =
                 , padding2 (px 8) zero
                 ]
             ]
-            [ text "© 2017–2019 Coredump Rapperswil-Jona"
+            [ text "© 2017–2020 Coredump Rapperswil-Jona"
             , text " | "
             , a [ href Routing.aboutPath ] [ text "About" ]
             , text " | "
