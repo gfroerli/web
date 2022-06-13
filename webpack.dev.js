ac34442f8b06cf5caa7e14c7fd3ed25b6332d962
Danilo Bargen <mail@dbrgn.ch>
2022-06-13 21:32:53 +0200

    Dev server: Fallback to webapp (#94)
    This allows URLs like /about to work.

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,9 @@ const common = require('./webpack.common.js');
 const config = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
+    devServer: {
+        historyApiFallback: true,
+    },
 });
 
 module.exports = config;
