dbe2b86dc500d0d6a4bd21883ddd01d98fd5348c
Danilo Bargen <mail@dbrgn.ch>
2023-01-06 19:42:16 +0100

    Bump copyright year

--- src/elm/Views.elm
+++ src/elm/Views.elm
@@ -283,7 +283,7 @@ mapView model =
                 , padding2 (px 8) zero
                 ]
             ]
-            [ text "© 2017–2022 Coredump Rapperswil-Jona"
+            [ text "© 2017–2023 Coredump Rapperswil-Jona"
             , text " | "
             , a [ href Routing.aboutPath ] [ text "About" ]
             , text " | "
